9ee5a8c9ca88eaa10e37334b8e3b0ab6ca6aee6f
watzmonium <ixisixi@gmail.com>
2024-02-25 11:01:39 -0500

    feat: completed openAPI yaml for create route

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
-import express from "express";
-import utils from "./utils/middleware";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
+import utils from './utils/middleware';
+import faceRouter from './routes/face-detection';
+import YAML from 'yamljs';
+import swaggerUI from 'swagger-ui-express';
+import path from 'path';
+
+const swaggerDoc = YAML.load(path.join(__dirname, './openAPI.yaml'));
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(utils.requestLogger);
-
+//app.use('/api/face_detection', faceRouter);
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
 app.use(utils.unknownEndpoint);
 app.use(utils.errorHandler);
 
-export default app
+export default app;
